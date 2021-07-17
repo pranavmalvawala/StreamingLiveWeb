bb7369211ceec5efdf74d00d673d10151b970fbf
Pranav Malvawala <42204892+pranavmalvawala@users.noreply.github.com>
2021-07-17 17:06:37 +0530

    use formik for validating registration form (#20)
    * update submodule
    
    * add formik and yup to dependencies
    
    * use formik in registration form

--- src/components/HomeRegister.tsx
+++ src/components/HomeRegister.tsx
@@ -1,97 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { ApiHelper, RegisterInterface, RoleInterface, LoginResponseInterface, RolePermissionInterface, ErrorMessages, ChurchInterface, UserInterface, EnvironmentHelper, PersonInterface, PasswordField } from ".";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Form, InputGroup, Button } from "react-bootstrap";
+import * as yup from "yup";
+import { Formik, FormikHelpers } from "formik";
 
-export const HomeRegister: React.FC = () => {
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
-  const [churchName, setChurchName] = React.useState("");
-  const [subDomain, setSubDomain] = React.useState("");
-  const [firstName, setFirstName] = React.useState("");
-  const [lastName, setLastName] = React.useState("");
-  const [errors, setErrors] = React.useState([]);
+const schema = yup.object().shape({
+  churchName: yup.string().required("Please enter your church name."),
+  firstName: yup.string().required("Please enter your first name."),
+  lastName: yup.string().required("Please enter your last name."),
+  password: yup.string().required("Please enter a password.").min(6, "Passwords must be at least 6 characters."),
+  email: yup.string().required("Please enter your email address.").email("Please enter a valid email address."),
+  subDomain: yup.string().required("Please select a subdomain for your church.")
+})
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const val = e.currentTarget.value;
-    switch (e.currentTarget.name) {
-      case "email": setEmail(val); break;
-      case "password": setPassword(val); break;
-      case "churchName": setChurchName(val); break;
-      case "subDomain": setSubDomain(val.toLowerCase().replaceAll(/[^a-z0-9]/ig, "")); break;
-      case "firstName": setFirstName(val); break;
-      case "lastName": setLastName(val); break;
-    }
-  }
+export function HomeRegister() {
+  const [customErrors, setCustomErrors] = React.useState([]);
 
-  const validate = async () => {
-    let errors = [];
-    if (churchName === "") errors.push("Please enter your church/organization name.");
-    if (subDomain === "") errors.push("Please select a subdomain for your site.");
-    if (email === "") errors.push("Please enter your email address.");
-    if (password === "") errors.push("Please enter a password.");
-    if (firstName === "") errors.push("Please enter your first name.")
-    if (lastName === "") errors.push("Please enter your last name.")
-    setErrors(errors);
-    return errors.length === 0;
-  }
-
-  const handleRegister = async (e: React.MouseEvent) => {
-    e.preventDefault();
-    const btn = e.currentTarget;
-    btn.innerHTML = "Validating..."
-    if (await validate()) {
-      btn.innerHTML = "Registering. Please wait..."
-      btn.setAttribute("disabled", "disabled");
-      // check if user already exist and if so, return user's associated churches
-      const verifyResponse = await ApiHelper.postAnonymous("/users/verifyCredentials", { email, password }, "AccessApi");
-      if (verifyResponse.errors !== undefined || verifyResponse.churches !== undefined) {
-        const errorMessage = <>There is already an account with this email address, please <a href={EnvironmentHelper.AccountsAppUrl}>login</a> to manage your churches and apps. If you wish to create a new church with this email, please register from <a href={EnvironmentHelper.ChurchAppUrl}>ChurchApps</a></>;
-        setErrors([errorMessage]);
-
-        btn.innerHTML = "Register"
-        btn.removeAttribute("disabled");
-
-        return;
-      }
-
-      let church: ChurchInterface = null;
-
-      //Create Access
-      let loginResp = await createAccess();
-
-      if (loginResp.errors?.length > 0) {
-        btn.innerHTML = "Register"
-        btn.removeAttribute("disabled");
-        return;
-      }
-
-      church = loginResp.churches.filter(c => c.subDomain === subDomain)[0];
-      if (church != null) {
-        btn.innerHTML = "Configuring..."
-        let resp: LoginResponseInterface = await ApiHelper.post("/churches/init", { appName: "StreamingLive" }, "StreamingLiveApi");
-        const { person }: { person: PersonInterface} = await ApiHelper.post("/churches/init", { user: loginResp.user }, "MembershipApi");
-        await ApiHelper.post("/userchurch", { personId: person.id }, "AccessApi");
-        if (resp.errors !== undefined) { setErrors(resp.errors); return 0; }
-        else {
-          window.location.href = EnvironmentHelper.SubUrl.replace("{key}", church.subDomain) + "/login/?jwt=" + ApiHelper.getConfig("AccessApi").jwt;
-        }
-      }
-    }
-    btn.innerHTML = "Register"
-    btn.removeAttribute("disabled");
-  }
-
-  const createAccess = async () => {
-    let data: RegisterInterface = { churchName, firstName, lastName, email, password, subDomain };
-
-    let resp: LoginResponseInterface = await ApiHelper.postAnonymous("/churches/register", data, "AccessApi");
-    if (resp.errors !== undefined) { setErrors(resp.errors); return resp; }
+  async function createAccess(values: RegisterInterface) {
+    let resp: LoginResponseInterface = await ApiHelper.postAnonymous("/churches/register", values, "AccessApi");
+    if (resp.errors !== undefined) { return resp; }
     else {
       const church = resp.churches[0];
       church.apis.forEach(api => { ApiHelper.setPermissions(api.keyName, api.jwt, api.permissions) });
       let response: LoginResponseInterface = await ApiHelper.post("/churchApps/register", { appName: "StreamingLive" }, "AccessApi");
-      if (response.errors !== undefined) { setErrors(response.errors); return null; }
+      if (response.errors !== undefined) { setCustomErrors(response.errors); return null; }
       else {
         const church = response.churches[0];
         church.apis.forEach(api => { ApiHelper.setPermissions(api.keyName, api.jwt, api.permissions) });
@@ -102,7 +35,7 @@ export const HomeRegister: React.FC = () => {
 
   }
 
-  const addHostRole = async (church: ChurchInterface, user: UserInterface) => {
+  async function addHostRole(church: ChurchInterface, user: UserInterface) {
     let role: RoleInterface = { churchId: church.id, name: "Hosts" };
     role.id = (await ApiHelper.post("/roles", [role], "AccessApi"))[0].id;
 
@@ -110,6 +43,38 @@ export const HomeRegister: React.FC = () => {
     permissions.push({ churchId: church.id, apiName: "MessagingApi", contentType: "Chat", action: "Host", roleId: role.id });
     await ApiHelper.post("/rolepermissions", permissions, "AccessApi");
   }
+
+  async function handleSubmit(values: RegisterInterface, { setSubmitting, setErrors }: FormikHelpers<RegisterInterface>) {
+    setCustomErrors([])
+    // check if user already exist and if so, return user's associated churches
+    const verifyResponse = await ApiHelper.postAnonymous("/users/verifyCredentials", { email: values.email, password: values.password }, "AccessApi");
+
+    if (verifyResponse.churches) {
+      const errorMessage = <>There is already an account with this email address, please <a href={EnvironmentHelper.AccountsAppUrl}>login</a> to manage your churches and apps. If you wish to create a new church with this email, please register from <a href={EnvironmentHelper.ChurchAppUrl}>ChurchApps</a></>;
+      setCustomErrors([errorMessage]);
+      return;
+    }
+
+    let church: ChurchInterface = null;
+    let loginResp = await createAccess(values);
+    if (loginResp.errors) {
+      setErrors({ subDomain: loginResp.errors[0] })
+      return;
+    }
+
+    church = loginResp.churches.filter(c => c.subDomain === values.subDomain)[0];
+    if (church != null) {
+      let resp: LoginResponseInterface = await ApiHelper.post("/churches/init", { appName: "StreamingLive" }, "StreamingLiveApi");
+      const { person }: { person: PersonInterface} = await ApiHelper.post("/churches/init", { user: loginResp.user }, "MembershipApi");
+      await ApiHelper.post("/userchurch", { personId: person.id }, "AccessApi");
+      if (resp.errors !== undefined) { setCustomErrors(resp.errors); return 0; }
+      else {
+        window.location.href = EnvironmentHelper.SubUrl.replace("{key}", church.subDomain) + "/login/?jwt=" + ApiHelper.getConfig("AccessApi").jwt;
+      }
+    }
+  }
+
+  const initialValues: RegisterInterface = { churchName: "", firstName: "", lastName: "", password: "", email: "", subDomain: "" };
 
   return (
     <>
@@ -125,40 +90,115 @@ export const HomeRegister: React.FC = () => {
               <p>If you would like to help support our mission of enabling churches to thrive with technology solutions, please consider <a href="https://livecs.org/partner/">partnering with us</a>.</p>
             </div>
             <div className="col-lg-6">
-              <ErrorMessages errors={errors} />
+              <ErrorMessages errors={customErrors} />
 
               <div id="registerBox">
-                <form method="post">
-                  <div className="form-group">
-                    <input type="text" name="churchName" value={churchName} className="form-control" placeholder="Church Name" onChange={handleChange} />
-                  </div>
-                  <div className="form-group">
-                    <div className="input-group">
-                      <input type="text" name="subDomain" className="form-control" placeholder="yourchurch" value={subDomain} onChange={handleChange} />
-                      <div className="input-group-append"><span className="input-group-text">.streaminglive.church</span></div>
-                    </div>
-                  </div>
-                  <Row>
-                    <Col>
-                      <div className="form-group">
-                        <input type="text" className="form-control" placeholder="First Name" name="firstName" value={firstName} onChange={handleChange} />
-                      </div>
-                    </Col>
-                    <Col>
-                      <div className="form-group">
-                        <input type="text" className="form-control" placeholder="Last Name" name="lastName" value={lastName} onChange={handleChange} />
-                      </div>
-                    </Col>
-                  </Row>
-                  <div className="form-group">
-                    <input type="text" name="email" value={email} className="form-control" placeholder="Email Address" onChange={handleChange} />
-                  </div>
-                  <PasswordField value={password} onChange={handleChange} containerClass="form-group" />
-                  <button className="btn btn-lg btn-primary btn-block" onClick={handleRegister}>Register</button>
-                </form>
+                <Formik
+                  validationSchema={schema}
+                  onSubmit={handleSubmit}
+                  initialValues={initialValues}
+                >
+                  {({
+                    handleSubmit,
+                    handleChange,
+                    values,
+                    touched,
+                    errors,
+                    isSubmitting
+                  }) => (
+                    <Form noValidate onSubmit={handleSubmit}>
+                      <Form.Group>
+                        <Form.Control
+                          type="text"
+                          placeholder="Church Name"
+                          name="churchName"
+                          value={values.churchName}
+                          onChange={handleChange}
+                          isInvalid={touched.churchName && !!errors.churchName}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                          {errors.churchName}
+                        </Form.Control.Feedback>
+                      </Form.Group>
+                      <Form.Group>
+                        <InputGroup>
+                          <Form.Control
+                            type="text"
+                            placeholder="yourchurch"
+                            name="subDomain"
+                            value={values.subDomain}
+                            onChange={handleChange}
+                            isInvalid={touched.subDomain && !!errors.subDomain}
+                          />
+                          <InputGroup.Text>.streaminglive.church</InputGroup.Text>
+                          <Form.Control.Feedback type="invalid">
+                            {errors.subDomain}
+                          </Form.Control.Feedback>
+                        </InputGroup>
+                      </Form.Group>
+                      <Row>
+                        <Col>
+                          <Form.Group>
+                            <Form.Control
+                              type="text"
+                              placeholder="First Name"
+                              name="firstName"
+                              value={values.firstName}
+                              onChange={handleChange}
+                              isInvalid={touched.firstName && !!errors.firstName}
+                            />
+                            <Form.Control.Feedback type="invalid">
+                              {errors.firstName}
+                            </Form.Control.Feedback>
+                          </Form.Group>
+                        </Col>
+                        <Col>
+                          <Form.Group>
+                            <Form.Control
+                              type="text"
+                              placeholder="Last Name"
+                              name="lastName"
+                              value={values.lastName}
+                              onChange={handleChange}
+                              isInvalid={touched.lastName && !!errors.lastName}
+                            />
+                            <Form.Control.Feedback type="invalid">
+                              {errors.lastName}
+                            </Form.Control.Feedback>
+                          </Form.Group>
+                        </Col>
+                      </Row>
+                      <Form.Group>
+                        <Form.Control
+                          type="email"
+                          placeholder="Email"
+                          name="email"
+                          value={values.email}
+                          onChange={handleChange}
+                          isInvalid={touched.email && !!errors.email}
+                        />
+
+                        <Form.Control.Feedback type="invalid">
+                          {errors.email}
+                        </Form.Control.Feedback>
+                      </Form.Group>
+                      <Form.Group>
+                        <PasswordField
+                          value={values.password}
+                          onChange={handleChange}
+                          isInvalid={touched.password && !!errors.password}
+                          errorText={errors.password}
+                        />
+                      </Form.Group>
+                      <Button type="submit" variant="primary" block disabled={isSubmitting}>
+                        {isSubmitting ? "Registering. Please wait..." : "Register for Free"}
+                      </Button>
+                    </Form>
+                  )}
+                </Formik>
                 <br />
                 <div>
-                                    Already have a site? <Link to="/login">Login</Link>
+                  Already have a site? <Link to="/login">Login</Link>
                 </div>
               </div>
             </div>
