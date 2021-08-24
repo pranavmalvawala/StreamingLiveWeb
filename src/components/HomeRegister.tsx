6e1b4e94ade575d7b5d743b5cb61425f0cd9a1ab
Pranav Malvawala <42204892+pranavmalvawala@users.noreply.github.com>
2021-08-24 23:55:07 +0530

    update registration form (#22)
    * update submodule
    
    * update registration form

--- src/components/HomeRegister.tsx
+++ src/components/HomeRegister.tsx
@@ -1,39 +1,30 @@
-import React from "react";
-import { HomeRegisterComplete } from "./HomeRegisterComplete";
-import { HomeRegisterForm } from "./HomeRegisterForm";
+import * as React from "react";
+import { ErrorMessages, EnvironmentHelper } from "./"
+import { Register } from "../appBase/pageComponents/components/Register"
 
 export function HomeRegister() {
-
-  const [subDomain, setSubDomain] = React.useState("");
-
-  const registrationCallBack = (selectedSubDomain: string) => {
-    setSubDomain(selectedSubDomain);
-  }
-
-  const getForm = () => {
-    if (subDomain) return (<HomeRegisterComplete subDomain={subDomain} />);
-    else return (<HomeRegisterForm registrationCallback={registrationCallBack} />);
-  }
+  const [customErrors, setCustomErrors] = React.useState<string[]>([]);
 
   return (
-    <>
-      <div id="register">
-        <div className="container">
-          <div className="text-center">
-            <h2 style={{ marginBottom: 20 }}>Register <span>Your Church</span></h2>
-          </div>
+    <div id="register">
+      <div className="container">
+        <div className="text-center">
+          <h2 style={{ marginBottom: 20 }}>Register <span>Your Church</span></h2>
+        </div>
 
-          <div className="row">
-            <div className="col-lg-6">
-              <p>This is a <b><u>completely free</u></b> service offered to churches by <a href="https://livecs.org/">Live Church Solutions</a>, a 501(c)(3) organization with EIN 45-5349618, that was founded in 2012 with the goal of helping small churches with their technical needs.</p>
-              <p>If you would like to help support our mission of enabling churches to thrive with technology solutions, please consider <a href="https://livecs.org/partner/">partnering with us</a>.</p>
-            </div>
-            <div className="col-lg-6">
-              {getForm()}
+        <div className="row">
+          <div className="col-lg-6">
+            <p>This is a <b><u>completely free</u></b> service offered to churches by <a href="https://livecs.org/">Live Church Solutions</a>, a 501(c)(3) organization with EIN 45-5349618, that was founded in 2012 with the goal of helping small churches with their technical needs.</p>
+            <p>If you would like to help support our mission of enabling churches to thrive with technology solutions, please consider <a href="https://livecs.org/partner/">partnering with us</a>.</p>
+          </div>
+          <div className="col-lg-6">
+            <ErrorMessages errors={customErrors} />
+            <div id="registerBox">
+              <Register updateErrors={setCustomErrors} appName="ChurchApps" appUrl={EnvironmentHelper.AccountsAppUrl} />
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
